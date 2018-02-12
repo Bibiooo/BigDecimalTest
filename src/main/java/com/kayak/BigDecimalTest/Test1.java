170e8f9b74d186eb9c385b426c79729f6fceb0e3
Bibio <474020826@qq.com>
2018-02-12 10:33:51 +0800

    First commit

---
+++ src/main/java/com/kayak/BigDecimalTest/Test1.java
@@ -0,0 +1,20 @@
+package com.kayak.BigDecimalTest;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by asus on 2018/2/11.
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        BigDecimal bd1 = new BigDecimal(10);
+        BigDecimal bd2 = new BigDecimal("3");
+        System.out.println(bd1);
+        System.out.println(bd2);
+        bd1 = bd1.divide(bd2,3,BigDecimal.ROUND_HALF_EVEN);
+        System.out.println(String.format("after db1:%s",bd1.toString()));
+        System.out.println(bd1.compareTo(bd2));
+    }
+
+}
